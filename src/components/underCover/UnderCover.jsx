b2ab8bff4a04dff218e649b16e32294fae3d39cb
marllonwagner <marllonwagner91@gmail.com>
2023-02-25 12:04:28 -0300

    under cover progress

---
+++ src/components/underCover/UnderCover.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './css/underCover.css'
+
+export default function UnderCover() {
+  return (
+    <section className='under-cover'>
+      <div className='presentation-container'>
+      <h3 className='presentation-title'>Clínica Odonto Karis</h3>
+      <div className='presentation-text-container'>
+        <p className='presentation-text'>Fundada em 2020, a clínica Odonto Karis tem como missão restaurar sorrisos e auto-estimas.</p>
+<p className='presentation-text'>Temos profissionais qualificados, prontos para atender suas necessidades. </p>
+</div>
+
+      </div>
+
+    </section>
+  )
+}
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Footer from '../../components/footer/Footer'
 import Header from '../../components/header/Header'
 import HomeCover from '../../components/homeCover/HomeCover'
+import UnderCover from '../../components/underCover/UnderCover'
 import './css/Home.css'
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
     <div>
       <Header/>
       <HomeCover/>
+      <UnderCover/>
       <Footer/>
     </div>
   )
