e88843c11fc3a8ce0b773fa50d77bbe42b815579
marllonwagner <marllonwagner91@gmail.com>
2024-03-27 13:39:34 -0300

    quee layout changes

--- src/components/quee/queeUpperCover/QueeUperCover.jsx
+++ src/components/quee/queeUpperCover/QueeUperCover.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import './css/queeUperCoverMob.css'
 import './css/queeUperCover.css'
+import ortoIcon2 from '../../../assets/icons/ortoIcon3.svg'
 
 export default function QueeUperCover() {
   return (
     <div className='quee-uper-cover'>
       <h1 className='fila-virtual'>Fila Virtual</h1>
+      <img 
+      className='orto-img-quee'
+      src={ortoIcon2} alt="orto icon" />
     </div>
   )
 }
